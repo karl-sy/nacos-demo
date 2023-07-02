528006b648e6291117615d288f6275d06e8f6b6e
karl.sy <17721084097@163.com>
2023-07-02 10:41:25 +0800

    Zookeeper本地启动

--- provider/deploy/build.sh
+++ provider/deploy/build.sh
@@ -3,6 +3,10 @@
 
 export REGISTRY=localhost:5001/
 
+
+# Zookeeper本地启动
+# docker run --name some-zookeeper -p 2181:2181 -d zookeeper:3.6.2
+
 #cd "$(dirname "$0")"
 
 docker build . -t ${REGISTRY}provider:1.0.0
